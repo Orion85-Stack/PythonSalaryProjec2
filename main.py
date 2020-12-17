160071ea76d66493eafa7a456f14c5b7205126e4
Orion85-Stack <70532381+Orion85-Stack@users.noreply.github.com>
2020-12-17 14:55:18 +0200

    updated the print layout for the hourly wages and added .title to the names

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ name_second = input("Please enter your second name: ")
 salary_list_names = []
 salary_list_names.append(name.title())
 salary_list_names.append(name_second.title())
-print (f"Herewith the names on the salary list: \n\t{salary_list_names}")
+print(f"Herewith the names on the salary list: \n\t{salary_list_names}")
 
-hourly_rate_name = int(input(f"What is the hourly rate for {name}: "))
-hourly_rate_name_second = int(input(f"What is the hourly rate for {name_second}: "))
+hourly_rate_name = int(input(f"What is the hourly rate for {name.title()}: "))
+hourly_rate_name_second = int(input(f"What is the hourly rate for {name_second.title()}: "))
 salary_list_names_wages = []
 salary_list_names_wages.append(hourly_rate_name)
 salary_list_names_wages.append(hourly_rate_name_second)
-print (salary_list_names_wages)
-print (f"The hourly wage for {name} is: \n\t{hourly_rate_name} \nand for {name_second} is: \n\t{hourly_rate_name_second}.")
+print(salary_list_names_wages)
+print(
+    f"The hourly wage for {name.title()} is: \n\t{hourly_rate_name} "
+    f"\nand for {name_second.title()} is: \n\t{hourly_rate_name_second}."
+)
