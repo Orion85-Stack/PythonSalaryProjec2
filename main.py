cf700722e508f3b5a1358c3e2f1d1d0c75879380
Orion85-Stack <70532381+Orion85-Stack@users.noreply.github.com>
2020-12-17 14:36:20 +0200

    add the list for the hourly wages and print function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 name = input("Please enter your name: ")
 name_second = input("Please enter your second name: ")
-salary_list = []
-salary_list.append(name.title())
-salary_list.append(name_second.upper())
-print (f"Herewith the names on the salary list: \n\t{salary_list}")
+salary_list_names = []
+salary_list_names.append(name.title())
+salary_list_names.append(name_second.title())
+print (f"Herewith the names on the salary list: \n\t{salary_list_names}")
+
+hourly_rate_name = int(input(f"What is the hourly rate for {name}: "))
+hourly_rate_name_second = int(input(f"What is the hourly rate for {name_second}: "))
+salary_list_names_wages = []
+salary_list_names_wages.append(hourly_rate_name)
+salary_list_names_wages.append(hourly_rate_name_second)
+print (salary_list_names_wages)
+print (f"The hourly wage for {name} is: \n\t{hourly_rate_name} \nand for {name_second} is: \n\t{hourly_rate_name_second}.")
